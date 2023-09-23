67144999c41df92cd832ce14bed415ef6914769a
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2023-09-22 20:23:41 -0400

    Update Get-ListeningProcesses.ps1

--- Get-ListeningProcesses.ps1
+++ Get-ListeningProcesses.ps1
@@ -1,111 +1,115 @@
-<#
-Proto localIP       localPort localPortName                foreignIP foreignPort State     isService procName                      procID
------ -------       --------- -------------                --------- ----------- -----     --------- --------                      ------
-TCP   0.0.0.0       80        HTTP                         0.0.0.0   0           LISTENING      True System                             4
-
-#>
-
-$Ports = @{
-    123 = @{ 'svchost.exe' = @{TCP = 'Network Time Protocol (NTP)'; UDP = 'Network Time Protocol (NTP)'}}
-    137 = @{ 'svchost.exe' = @{TCP = 'NETBIOS Name Service'; UDP = 'NETBIOS Name Service'}}
-    138 = @{ 'svchost.exe' = @{TCP = 'NETBIOS Datagram Service'; UDP = 'NETBIOS Datagram Service'}}
-    139 = @{ 'svchost.exe' = @{TCP = 'NETBIOS Session Service'; UDP = 'NETBIOS Session Service'}}
-    445 = @{ 'svchost.exe' = @{TCP = 'SMB (Server Message Block)'; UDP = 'SMB (Server Message Block)'}}
-    3389 = @{ 'svchost.exe' = @{TCP = 'Microsoft Terminal Server (RDP)'; UDP = 'Microsoft Terminal Server (RDP)'}}
-    4500 = @{ 'svchost.exe' = @{UDP = 'IPSec - NAT traversal';}}
-    5985 = @{ 'svchost.exe' = @{UDP = 'WinRM 2.0';}}
-}
-
-Try {
-    $test = $Ports[137]['svchost.exe']['TCP']
-} Catch {
-    
-}
-
+# Use a custom class to enforce strict data typing.
 Class ListeningPort {
-    [String]$Proto
-    [String]$LocalIP
-    [Int]   $localPort
-    [String]$localPortName
+    #Initial Required Values
+    [String]$Protocol
+    [String]$LocalAddress
+    [Int]   $LocalPort
+    [int]   $ProcessID
+    [String]$RemoteAddress
+
+    # Default Values
     [String]$State = 'Listening'
+    
+    # Calculated Values
+    [String]$localPortName
     [Bool]  $isService = $False
     [String]$ProcessName
-    [String]$ProcessID
     [String]$ProcessPath
 
-    #Constructor
     ListeningPort(
-        [String]$Proto,
-        [String]$LocalIP,
-        [Int]   $localPort,
-        [String]$ProcessID,
-        [Array] $Processes,
-        [Array] $Services
+        [String]$Protocol,
+        [PSCustomObject]$InputObject
     ) {
-    
-        Write-Verbose -Message:'Loading process information.'
-        $Process = $Processes.Where({$_.ProcessId -eq $ProcessId})
+        $This.Protocol = $Protocol.ToUpper()
+        $This.LocalAddress = $InputObject.LocalAddress
+        $This.LocalPort = $InputObject.LocalPort
+        $This.ProcessID = $InputObject.OwningProcess
+    }
+}
+Class Processes {
+    [String]$ProcessName
+    [String]$ProcessID
+    [System.IO.FileInfo]$ProcessPath
 
-        # Error handling for process.
-        If ([string]::IsNullOrEmpty($Process) -eq -$true) {
-            Throw 'Unable to locate a matching process; exiting.'
-            exit 1
-        }
+    Processes(
+        [CimInstance]$Process
+    ){
+        $This.ProcessID = $Process.ProcessID
 
-        Write-Verbose -Message:'Finding services with matching ProcessID.'
-        $Service = $Services.Where({$_.ProcessId -eq $ProcessId})
-    
-        # If no services were found, then this is empty so invert.
-        $this.isService = -NOT [String]::IsNullOrEmpty($Service)
-    
-        # Some mundane settings.
-        $this.Proto = $Proto
-        $this.LocalIP = $LocalIP
-        $This.localPort = $LocalPort
-        $this.ProcessID = $ProcessID
-        $this.ProcessName = $process.Name
-
-        If ($This.isService -eq $True) {
-            If ([String]::IsNullOrEmpty($This.ProcessPath) -and $This.isService -eq $True) {
+        Switch ($This.ProcessID) {
+            4 {
+                $This.ProcessName = 'System'
+            }
+            Default {
                 Try {
-                $this.ProcessPath = [Regex]::Match($Service.PathName,'^.*?(\b\w:\\.*?.exe\b)') |
-                    Select-Object -ExpandProperty:'Groups' |
-                    Select-Object -Skip:1 -ExpandProperty:'Value'
+                    $ProcessString = $Process.ExecutablePath,$Process.CommandLine -join ' '
+                    $this.ProcessPath = [Regex]::Match($ProcessString,'^.*?(\b\w:\\.*?.exe\b)') |
+                        Select-Object -ExpandProperty:'Groups' |
+                        Select-Object -Skip:1 -ExpandProperty:'Value'
                 } Catch {
-                    Write-Verbose -Message:'Failed to find path match.'
+                    Write-Host -Message:'Unable to match.'
                 }
+                $This.ProcessName = ([System.IO.FileInfo]$Process.Name).BaseName
             }
-        } ElseIf ($Null -NE $process.ExecutablePath) {
-            Try {
-                $this.ProcessPath = ([System.IO.FileInfo]$process.ExecutablePath).FullName
-            } Catch {
-                Write-Warning -Message:'Unable to convert ExecutablePath to fullname.'
-            }
-        } Else {
-            $this.ProcessPath = Get-Process -Id:$ProcessID | Select-Object -ExpandProperty:'Path'
         }
+
+        
     }
 }
 
-New-Variable -Force -Name:'Results' -Value:(New-Object -TypeName:'System.Collections.ArrayList')
-New-Variable -Force -Name:'CimSession' -Value:(New-CimSession -ComputerName:'LOCALHOST' `
-    -SessionOption:(New-CimSessionOption -Protocol:'DCOM'))
-
-New-Variable -Force -Name:'Processes' -Value:(Get-CimInstance -CimSession:$cimSession `
-    -Namespace:'root/cimV2' -ClassName:'Win32_Process' -Property:('ProcessID',
-    'ExecutablePath','Name','*')
+Write-Verbose -Message:'Building reusable DCOM CimSession.'
+New-Variable -Force -Name:'CIMSession' -Value:(
+     New-CimSession -ComputerName:'LOCALHOST' -SessionOption:(
+        New-CimSessionOption -Protocol:'DCOM'
+    )
 )
 
-New-Variable -Force -Name:'Services' -Value:(Get-CimInstance -CimSession:$cimSession `
-    -Namespace:'root/cimV2' -ClassName:'Win32_Service' -Filter:"(State <> 'Stopped')" `
-    -Property:('Name','ProcessId','State','PathName')
-)
 
-# Query TCP Ports
-Get-NetTCPConnection -State:'Listen' | ForEach-Object -Process:{
-    $Null = $Results.Add([ListeningPort]::NEW('TCP',$_.LocalAddress,$_.LocalPort,$_.OwningProcess,$Processes,$Services))
-}
 
-Get-NetUDPEndpoint | ForEach-Object -Process:{
-    $Null = $Results.Add([ListeningPort]::NEW('UDP',$_.LocalAddress,$_.LocalPort,$_.OwningProcess,$Processes,$Services))
+New-Variable -Force -Name:'listeningPorts' -Value:(New-Object -TypeName:'System.Collections.ArrayList')
+
+Write-Verbose -Message:'Get TCP & UDP listening ports.'
+$PortSplat = @{Property = 'LocalAddress','LocalPort','OwningProcess','RemoteAddress'}
+
+Write-Verbose -Message:'Getting TCP listening ports.'
+$_tcpPorts = Get-NetTCPConnection -State:'Listen' | Select-Object @PortSplat
+$_tcpPorts | & { Process { $Null = $listeningPorts.Add([ListeningPort]::new('TCP',$_)) } }
+
+Write-Verbose -Message:'Getting UDP listening ports.'
+$_udpPorts = Get-NetUDPEndpoint | Select-Object @PortSplat
+$_udpPorts | & { Process { $Null = $listeningPorts.Add([ListeningPort]::new('UDP',$_)) } }
+
+# region ======= [ Load System Process Information ] =========================================================== #
+'','','' | & { Process {}}
+
+# endregion ==== [ Load System Process Information ] =========================================================== #
+
+Function Initialize-Variable {
+    Param (
+        [Parameter(Mandatory = $True, ParameterSetName = 'Default')]
+        [String]$Name,
+        [Parameter(Mandatory = $True, ParameterSetName = 'Hashtable')]
+        [Hashtable]$Variable
+    )
+
+    If ($PSCmdlet.ParameterSetName -eq 'Default') {
+        $Value = $Null
+    } Else {
+        $Name = $Variable.Keys[0]
+        $Value = $Variable.Values[0]
+    }
+    New-Variable -Force -Scope:1 -Name:$Name -Value:$Value
+
 }
+New-Variable -Force -Name:'Processes' -Value:(New-Object -TypeName:'System.Collections.ArrayList')
+New-Variable -Force -Name:'_CimProcesses' -Value:(
+New-Variable -Force -Name:'_GetProcesses' -Value:(
+
+Get-CimInstance -CimSession:$CIMSession -Namespace:'root/cimV2' -ClassName:'Win32_Process' `
+    -Property:('ProcessID','ExecutablePath','Name','CommandLine','*') |
+        Where-Object -FilterScript:{
+            $_.ProcessId -in $listeningPorts.ProcessID
+        } |
+        & { Process { $Null = $Processes.Add([Processes]::new($_)) } }
+
+$Processes
